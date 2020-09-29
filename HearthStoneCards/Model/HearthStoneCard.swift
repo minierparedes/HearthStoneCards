42604d1735972e362bfade3c4e5a0018c871ec88
minierparedes <3thancr0wn@gmail.com>
2020-09-29 10:38:54 +0900

    Deleted: URL+Extention, updated: HearthStoneCard model, Services: fetch API call

---
+++ HearthStoneCards/Model/HearthStoneCard.swift
@@ -0,0 +1,28 @@
+//
+//  HearthStoneCard.swift
+//  HearthStoneCards
+//
+//  Created by ethancr0wn on 2020/09/29.
+//  Copyright © 2020 ethancr0wn. All rights reserved.
+//
+
+import Foundation
+
+struct HearthStoneCard: Codable, Identifiable {
+    var id:String?
+    var name:String?
+    var text:String?
+    var flavor:String?
+    var artist:String?
+    var attack:Int?
+    var cardClass:String?
+    var collectible:Bool?
+    var cost:Int?
+    var elite:Bool?
+    var faction:String?
+    var health:Int?
+    var rarity:String?
+    var set:String?
+    var type:String?
+    var mechanics: [String]?
+}
